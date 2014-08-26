1df2848df5664f7183a6e17c6761c37fa3aafdd6
Jathu Satkunarajah <jathu.satkunarajah@gmail.com>
2014-08-26 17:40:23 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,7 @@
+var http = window.location.href, position = http.indexOf("m.wikipedia");
+
+if(position < 0) {
+	position = http.indexOf(".wikipedia");
+	http = http.substring(0, position)+".m"+http.substring(position, http.length);
+	window.location.href = http;
+}
